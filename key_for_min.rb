5146e8624a8bdd2835b879e96b33fd9e8b98219f
Chris Aporta <christopher.aporta@gmail.com>
2015-09-01 23:27:18 -0400

    complete lab

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,9 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash != {}
+    min_pair = name_hash.reject { |key, val| name_hash.any? { |key2, val2| val2 < val } }
+    min_pair.keys[0]
+  else
+    nil
+  end
 end
